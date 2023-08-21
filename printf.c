cfcf545441cd1ebb677ad517c60595fa02e32ff7
jaffycoder <abubakarjafaru1@gmail.com>
2023-08-21 14:09:27 +0000

    added printf

--- printf.c
+++ printf.c
@@ -2,58 +2,51 @@
 #include <unistd.h>
 #include <stdarg.h>
 
+/**
+ * _printf - Custom printf function.
+ * @format: The format string.
+ * Return: Number of characters printed.
+ */
 int _printf(const char *format, ...)
 {
-	int character_printed = 0;
-	va_list list_of_arguments;
+	int char_printed = 0, str_len;
+	va_list args;
 
-	if (format == NULL)
+		if (!format)
 		return (-1);
-
-	va_start(list_of_arguments, format);
+	va_start(args, format);
 
 	while (*format)
 	{
-		if (*format != '%')
-		{
-			write(1, format, 1);
-			character_printed++;
-		}
-		else
-		{
-			format++;
-			if (*format == '\0')
-				break;
-
-			if (*format == '%')
-			{
-				write(1, format, 1);
-				character_printed++;
-			}
-			else if (*format == 'c')
-			{
-				char c = va_arg(list_of_arguments, int);
-
-				write(1, &c, 1);
-				character_printed++;
-			}
-			else if (*format == 's')
-			{
-				char *str = va_arg(list_of_arguments, char*);
-				int str_len = 0;
-
-				while (str[str_len] != '\0')
-					str_len++;
-
-				write(1, str, str_len);
-				character_printed += str_len;
-			}
-		}
-
+	if (*format != '%')
+	{
+		write(1, format, 1);
+		char_printed++;
+	}
+	else
+	{
 		format++;
+		if (*format == '\0')
+		break;
+	if (*format == '%' || *format == 'c')
+	{
+		write(1, format, 1);
+		char_printed++;
+	}
+	else if (*format == 's')
+	{
+		char *str = va_arg(args, char*);
+
+		str_len = 0;
+		while (str[str_len])
+			str_len++;
+		write(1, str, str_len);
+		char_printed += str_len;
+	}
+	}
+	format++;
 	}
 
-	va_end(list_of_arguments);
-
-	return (character_printed);
+	va_end(args);
+	return (char_printed);
 }
