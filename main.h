2160211453e6be1ae559e89d02bdabe8f97b12bf
jaffycoder <abubakarjafaru1@gmail.com>
2023-08-21 13:29:00 +0000

    added printf

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <string.h>
+#include <unistd.h>
+/*
+ * comments for main.h.
+ */
+
+int _printf(const char *format, ...);
+int binary(int num);
+int octal(int num);
+
+#endif
---
+++ printf.c
@@ -0,0 +1,59 @@
+#include "main.h"
+#include <unistd.h>
+#include <stdarg.h>
+
+int _printf(const char *format, ...)
+{
+	int character_printed = 0;
+	va_list list_of_arguments;
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(list_of_arguments, format);
+
+	while (*format)
+	{
+		if (*format != '%')
+		{
+			write(1, format, 1);
+			character_printed++;
+		}
+		else
+		{
+			format++;
+			if (*format == '\0')
+				break;
+
+			if (*format == '%')
+			{
+				write(1, format, 1);
+				character_printed++;
+			}
+			else if (*format == 'c')
+			{
+				char c = va_arg(list_of_arguments, int);
+
+				write(1, &c, 1);
+				character_printed++;
+			}
+			else if (*format == 's')
+			{
+				char *str = va_arg(list_of_arguments, char*);
+				int str_len = 0;
+
+				while (str[str_len] != '\0')
+					str_len++;
+
+				write(1, str, str_len);
+				character_printed += str_len;
+			}
+		}
+
+		format++;
+	}
+
+	va_end(list_of_arguments);
+
+	return character_printed;
+}
